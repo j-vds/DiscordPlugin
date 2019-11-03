b121bcdb3180b61c4bbaa468915504b39163bd62
J-VdS <jeroen.vanderstraeten@ugent.be>
2019-11-03 23:29:09 +0100

    thx anuke

--- src/main/java/disc/command/comCommands.java
+++ src/main/java/disc/command/comCommands.java
@@ -27,7 +27,7 @@ public class comCommands implements MessageCreateListener {
         else if (event.getMessageContent().equalsIgnoreCase("..players")){
             StringBuilder lijst = new StringBuilder();
             lijst.append("players: " + Vars.playerGroup.size()+"\n");
-            lijst.append("online admins: " + Vars.playerGroup.all().count(p->p.isAdmin)+"\n");
+            //lijst.append("online admins: " + Vars.playerGroup.all().count(p->p.isAdmin)+"\n");
             for (Player p :Vars.playerGroup.all()){
                 lijst.append("* " + p.name.trim() + "\n");
             }
@@ -35,13 +35,18 @@ public class comCommands implements MessageCreateListener {
         }
         //info
         else if (event.getMessageContent().equalsIgnoreCase("..info")){
-            StringBuilder lijst = new StringBuilder();
-            lijst.append("map: "+ Vars.world.getMap().name() + "\n" + "author: " + Vars.world.getMap().author() + "\n");
-            lijst.append("wave: " + Vars.state.wave + "\n");
-            lijst.append("enemies: " + Vars.state.enemies + "\n");
-            lijst.append("players: " + Vars.playerGroup.size() + '\n');
-            lijst.append("admins (online): " + Vars.playerGroup.all().count(p->p.isAdmin));
-            new MessageBuilder().appendCode("", lijst.toString()).send(event.getChannel());
+            try {
+                StringBuilder lijst = new StringBuilder();
+                lijst.append("map: " + Vars.world.getMap().name() + "\n" + "author: " + Vars.world.getMap().author() + "\n");
+                lijst.append("wave: " + Vars.state.wave + "\n");
+                lijst.append("enemies: " + Vars.state.enemies + "\n");
+                lijst.append("players: " + Vars.playerGroup.size() + '\n');
+                //lijst.append("admins (online): " + Vars.playerGroup.all().count(p -> p.isAdmin));
+                new MessageBuilder().appendCode("", lijst.toString()).send(event.getChannel());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
         }
         //infores, werkt enkel als er minstens 1 speler online is!
         else if (event.getMessageContent().equalsIgnoreCase("..infores")){
